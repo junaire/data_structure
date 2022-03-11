9514e50b7d60a3b5596fdcec4643e870abba1826
Jun Zhang <jun@junz.org>
2022-03-11 18:28:55 +0800

    Add DFS and BFS for ListGraph implementation.
    Signed-off-by: Jun Zhang <jun@junz.org>

--- graph/graph.c
+++ graph/graph.c
@@ -1,5 +1,6 @@
 #include "graph.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,7 +13,7 @@ static Edge* jun_edge_create(int start, int end, int weight) {
   return edge;
 }
 
-static void jun_edge_destroy(Edge* edge) {
+void jun_edge_destroy(Edge* edge) {
   free(edge);
   edge = NULL;
 }
@@ -25,9 +26,7 @@ static BaseGraph* jun_base_graph_create_internal(int vertex_number,
   base_graph->kind = kind;
 
   base_graph->marks = malloc(sizeof(VisitFlag) * vertex_number);
-  for (int i = 0; i < vertex_number; ++i) {
-    base_graph->marks[i] = Unvisited;
-  }
+  jun_graph_clear_visit_flag(base_graph);
 
   return base_graph;
 }
@@ -36,6 +35,15 @@ static void jun_base_graph_destroy_internal(BaseGraph* base_graph) {
   free(base_graph->marks);
   free(base_graph);
   base_graph = NULL;
+}
+
+static Vertex* jun_vertex_create(int index, TYPE data) {
+  Vertex* vertex = malloc(sizeof(Vertex));
+  vertex->data = data;
+  vertex->index = index;
+  vertex->next = NULL;
+
+  return vertex;
 }
 
 AdjGraph* jun_adj_graph_create(int vertex_number, GraphKind kind) {
@@ -82,12 +90,14 @@ Edge* jun_adj_graph_next_edge(AdjGraph* adj_graph, Edge* edge) {
 
 void jun_adj_graph_set_edge(AdjGraph* adj_graph, int start, int end,
                             int weight) {
+  // if there's already an edge, add one.
   if (adj_graph->matrix[start][end] == 0) {
     adj_graph->base_graph->edge_number++;
   }
 
   adj_graph->matrix[start][end] = weight;
 
+  // note we should add another if the type of graph is undirected.
   if (adj_graph->base_graph->kind == Undirected) {
     adj_graph->matrix[end][start] = weight;
   }
@@ -101,5 +111,139 @@ void jun_adj_graph_delete_edge(AdjGraph* adj_graph, int start, int end) {
 
   if (adj_graph->base_graph->kind == Undirected) {
     adj_graph->matrix[end][start] = 0;
+  }
+}
+
+void jun_adj_graph_print(AdjGraph* adj_graph) {
+  for (int i = 0; i < adj_graph->base_graph->vertex_number; ++i) {
+    for (int j = 0; j < adj_graph->base_graph->vertex_number; ++j) {
+      printf("%d", adj_graph->matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+ListGraph* jun_list_graph_create(int vertex_number, GraphKind kind) {
+  ListGraph* list_graph = malloc(sizeof(ListGraph));
+  list_graph->base_graph = jun_base_graph_create_internal(vertex_number, kind);
+  list_graph->vertex_list = malloc(sizeof(Edge*) * vertex_number);
+
+  return list_graph;
+}
+
+void jun_list_graph_destroy(ListGraph* list_graph) {
+  jun_base_graph_destroy_internal(list_graph->base_graph);
+
+  for (int i = 0; i < list_graph->base_graph->vertex_number; ++i) {
+    Vertex* iter = list_graph->vertex_list[i];
+    while (iter != NULL) {
+      Vertex* tmp = iter->next;
+      free(iter);
+      iter = tmp;
+    }
+  }
+}
+
+static void jun_list_graph_set_edge_internal(ListGraph* list_graph, int beg,
+                                             int end, TYPE data) {
+  Vertex* iter = list_graph->vertex_list[beg];
+  if (iter->next == NULL) {
+    iter->next = jun_vertex_create(end, data);
+  } else {
+    Vertex* tmp = iter->next;
+    Vertex* edge = jun_vertex_create(end, data);
+    iter->next = edge;
+    edge->next = tmp;
+  }
+}
+
+static void jun_list_graph_delete_edge_internal(ListGraph* list_graph, int beg,
+                                                int end) {
+  // TODO
+}
+
+void jun_list_graph_set_edge(ListGraph* list_graph, int beg, int end) {
+  jun_list_graph_set_edge_internal(list_graph, beg, end,
+                                   list_graph->vertex_list[end]->data);
+
+  if (list_graph->base_graph->kind == Undirected) {
+    jun_list_graph_set_edge_internal(list_graph, end, beg,
+                                     list_graph->vertex_list[beg]->data);
+  }
+}
+
+void jun_list_graph_delete_edge(ListGraph* list_graph, int beg, int end) {
+  jun_list_graph_delete_edge_internal(list_graph, beg, end);
+  if (list_graph->base_graph->kind == Undirected) {
+    jun_list_graph_delete_edge_internal(list_graph, end, beg);
+  }
+}
+
+void jun_list_graph_print(ListGraph* list_graph) {
+  for (int i = 0; i < list_graph->base_graph->vertex_number; ++i) {
+    Vertex* vertex = list_graph->vertex_list[i];
+
+    while (vertex != NULL) {
+      printf("%c", vertex->data);
+      vertex = vertex->next;
+    }
+    printf("\n");
+  }
+}
+
+void jun_list_graph_set_vertex(ListGraph* list_graph, TYPE* vertex_arr) {
+  for (int i = 0; i < list_graph->base_graph->vertex_number; ++i) {
+    list_graph->vertex_list[i] = jun_vertex_create(i, vertex_arr[i]);
+  }
+}
+
+void jun_list_graph_bfs(ListGraph* list_graph,
+                        void (*visitor)(Vertex* vertex)) {
+  Vertex* iter = list_graph->vertex_list[0];
+  Vertex* tmp = NULL;
+  if (iter->next) {
+    tmp = iter->next;
+  }
+
+  for (int i = 1; i < list_graph->base_graph->vertex_number; ++i) {
+    if (iter->next) {
+      tmp = iter->next;
+    }
+    while (iter != NULL) {
+      if (list_graph->base_graph->marks[iter->index] == Unvisited) {
+        visitor(iter);
+        list_graph->base_graph->marks[iter->index] = Visited;
+      }
+      iter = iter->next;
+    }
+
+    iter = list_graph->vertex_list[tmp->index];
+  }
+}
+
+void jun_list_graph_dfs(ListGraph* list_graph,
+                        void (*visitor)(Vertex* vertex)) {
+  int start = 0;
+
+  for (int i = 0; i < list_graph->base_graph->vertex_number; ++i) {
+    if (list_graph->base_graph->marks[start] == Unvisited) {
+      visitor(list_graph->vertex_list[start]);
+      list_graph->base_graph->marks[start] = Visited;
+
+      Vertex* iter = list_graph->vertex_list[start]->next;
+      while (iter != NULL) {
+        if (list_graph->base_graph->marks[iter->index] == 0) {
+          start = iter->index;
+          break;
+        }
+        iter = iter->next;
+      }
+    }
+  }
+}
+
+void jun_graph_clear_visit_flag(BaseGraph* graph) {
+  for (int i = 0; i < graph->vertex_number; ++i) {
+    graph->marks[i] = Unvisited;
   }
 }
--- graph/graph.h
+++ graph/graph.h
@@ -1,29 +1,53 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
 
+#define TYPE char
+
 typedef enum { Directed, Undirected } GraphKind;
 
 typedef enum { Unvisited, Visited } VisitFlag;
 
-typedef struct Edge {
+// used in adj_graph implementation
+typedef struct {
   int start;
   int end;
-  // FIXME: use generic data type
   int weight;
 } Edge;
+
+// used in list_graph implementation
+typedef struct Vertex {
+  struct Vertex* next;
+  int index;
+  TYPE data;
+} Vertex;
 
 typedef struct {
   int vertex_number;
   int edge_number;
   VisitFlag* marks;
   GraphKind kind;
-
 } BaseGraph;
 
 typedef struct {
   BaseGraph* base_graph;
   int** matrix;
 } AdjGraph;
+
+typedef struct {
+  BaseGraph* base_graph;
+
+  // a list of linked list
+  //
+  // |0|-> |5|-> |6|-> NULL
+  // |----
+  // |1|-> |7|-> |1|-> NULL
+  // |----
+  // |2|-> |2|-> |8|-> NULL
+  // |----
+  // |3|-> |4|-> |9|-> NULL
+  //
+  Vertex** vertex_list;
+} ListGraph;
 
 static BaseGraph* jun_base_graph_create_internal(int vertex_number,
                                                  GraphKind kind);
@@ -32,7 +56,9 @@ static void jun_base_graph_destroy_internal(BaseGraph* base_graph);
 
 static Edge* jun_edge_create(int start, int end, int weight);
 
-static void jun_edge_destroy(Edge* edge);
+void jun_edge_destroy(Edge* edge);
+
+static Vertex* jun_vertex_create(int index, TYPE data);
 
 AdjGraph* jun_adj_graph_create(int vertex_number, GraphKind kind);
 
@@ -46,4 +72,35 @@ void jun_adj_graph_set_edge(AdjGraph* adj_graph, int start, int end,
                             int weight);
 
 void jun_adj_graph_delete_edge(AdjGraph* adj_graph, int start, int end);
+
+void jun_adj_graph_print(AdjGraph* adj_graph);
+
+ListGraph* jun_list_graph_create(int vertex_number, GraphKind kind);
+
+void jun_list_graph_destroy(ListGraph* list_graph);
+
+Edge* jun_list_edge_first_edge(ListGraph* list_graph, int one_vertex);
+
+Edge* jun_list_graph_next_edge(ListGraph* list_graph, Edge* edge);
+
+void jun_list_graph_set_edge(ListGraph* list_graph, int beg, int end);
+
+void jun_list_graph_delete_edge(ListGraph* list_graph, int start, int end);
+
+void jun_list_graph_set_vertex(ListGraph* list_graph, TYPE* vertex_arr);
+
+void jun_list_graph_print(ListGraph* list_graph);
+
+static void jun_list_graph_set_edge_internal(ListGraph* list_graph, int beg,
+                                             int end, TYPE data);
+
+static void jun_list_graph_delete_edge_internal(ListGraph* list_graph, int beg,
+                                                int end);
+
+void jun_list_graph_bfs(ListGraph* list_graph, void (*visitor)(Vertex* vertex));
+
+void jun_list_graph_dfs(ListGraph* list_graph, void (*visitor)(Vertex* vertex));
+
+void jun_graph_clear_visit_flag(BaseGraph* base_graph);
+
 #endif
